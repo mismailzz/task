69f3fa818f6530885c200be760606e689a9addb8
Muhammad Ismail Zam Zam Butt <mismailzz75@gmail.com>
2025-12-10 20:35:53 +0100

    taking args in add.go and do.go

--- cmd/add.go
+++ cmd/add.go
@@ -1,18 +1,25 @@
 package cmd
 
 import (
-    "fmt"
-    "github.com/spf13/cobra"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
 )
 
 var AddCmd = &cobra.Command{
-    Use:   "add",
-    Short: "add a new task",
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Adding a new task...")
-    },
+	Use:   "add",
+	Short: "add a new task",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) <= 0 {
+			fmt.Println("Please provide a task description.")
+			return
+		}
+		taskDescription := strings.Join(args, " ")
+		fmt.Printf("Task added: %s\n", taskDescription)
+	},
 }
 
 func init() {
-    RootCmd.AddCommand(AddCmd)
+	RootCmd.AddCommand(AddCmd)
 }
--- cmd/do.go
+++ cmd/do.go
@@ -1,18 +1,24 @@
 package cmd
 
 import (
-    "fmt"
-    "github.com/spf13/cobra"
+	"fmt"
+
+	"github.com/spf13/cobra"
 )
 
 var DoCmd = &cobra.Command{
-    Use:   "do",
-    Short: "mark a task as done",
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Marking a task as done...")
-    },
+	Use:   "do",
+	Short: "mark a task as done",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) <= 0 {
+			fmt.Println("Please provide a task number.")
+			return
+		}
+		taskNumber := args[0]
+		fmt.Printf("Task %s marked as done.\n", taskNumber)
+	},
 }
 
 func init() {
-    RootCmd.AddCommand(DoCmd)
+	RootCmd.AddCommand(DoCmd)
 }
